71bc8ec8e4610ad0087fb0b955c3c24616e81dad
Alexander <alexanderl96@yahoo.com>
2022-05-11 11:16:38 -0400

    getting db from localhost

--- db/dbconfig.js
+++ db/dbconfig.js
@@ -2,7 +2,7 @@ const pgp = require('pg-promise')(); //npm install pg-promise
 require("dotenv").config();
 const db_Url = process.env.DB_URL; // set this variable in .env
 const cn = {
-    connectionString: 'https://my-baseball-teams.herokuapp.com',
+    connectionString: db_Url,
     allowExitOnIdle: true,
     max: 30
 }
