010c82f086088b3f1b7497e2cf69500508ff1581
Alexander <alexanderl96@yahoo.com>
2022-09-15 18:17:13 -0400

    set up .env confog

--- db/dbconfig.js
+++ db/dbconfig.js
@@ -1,11 +1,25 @@
-const pgp = require('pg-promise')(); //npm install pg-promise
+const pgp = require("pg-promise")();
 require("dotenv").config();
-const db_Url = process.env.DB_URL; // set this variable in .env
-const cn = {
-    connectionString: db_Url,
-    allowExitOnIdle: true,
-    max: 30
-}
+
+const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD } =
+  process.env;
+
+const cn = DATABASE_URL
+  ? {
+      connectionString: DATABASE_URL,
+      max: 30,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      host: PG_HOST,
+      port: PG_PORT,
+      database: PG_DATABASE,
+      user: PG_USER,
+      password: PG_PASSWORD,
+    };
+
 const db = pgp(cn);
 
-module.exports = db; 
+module.exports = db;
